a636c94c7883d9b252ba6892949cf974b1cf959d
GuyFromJupiter <sukumarneelam06@gmail.com>
2026-05-22 16:43:05 +0530

    Add project files.

---
+++ PhotoDuplicateDetector/HashLayer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoDuplicateDetector
+{
+
+    public class HashLayer
+    {
+
+        internal static Dictionary<string, List<string>> hashMap = new Dictionary<string, List<string>>();
+
+        public static void Sub1()
+        {
+            foreach (string path in ImageScanner.GetData())
+            {
+                string? hash = ComputeSha256Hash(path);
+                if (hash == null) continue;
+
+                if (!hashMap.TryGetValue(hash, out var list))
+                {
+                    list = new List<string>();
+                    hashMap[hash] = list;
+                }
+
+                list.Add(path);
+            }
+
+            Sha256Compare();
+        }
+        static string? ComputeSha256Hash(string filePath)
+        {
+            try
+            {
+                using var sha256 = SHA256.Create();
+                using var stream = File.OpenRead(filePath);
+
+                byte[] hash = sha256.ComputeHash(stream);
+                return Convert.ToHexString(hash);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Hash failed for {filePath}: {e.Message}");
+                return null;
+            }
+        }
+        public static void Sha256Compare()
+        {
+            bool duplicateFound = false;
+            foreach (var record in hashMap)
+            {
+                if (record.Value.Count > 1)
+                {
+                    duplicateFound = true;
+                    Console.WriteLine($"Duplicate found for hash: {record.Key}");
+                    foreach (var path in record.Value)
+                    {
+                        Console.WriteLine($" - {path}");
+                    }
+                }
+
+            }
+            if (!duplicateFound)
+            {
+                Console.WriteLine("No duplicates found.");
+            }
+        }
+    }
+}
---
+++ PhotoDuplicateDetector/ImageScanner.cs
@@ -0,0 +1,139 @@
+﻿// For Directory.GetFiles and Directory.GetDirectories
+// For File.Exists, Directory.Exists
+
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PhotoDuplicateDetector
+{
+    public class ImageScanner
+    {
+        //hard coded list of image paths, the list is strictly of type string
+        //this list will store every image path
+        private static List<string> ImagePath = new List<string>();
+        private static List<string> InputPath = new List<string>();
+
+        public static void Main(string[] args)
+        {
+            // Get the path to the directory or file to process from the user.
+            GetNumberOfInputPaths();
+            InputPath.Clear();
+            ImagePath.Clear();
+            HashLayer.hashMap.Clear();
+            foreach (string path in InputPath)
+            {
+                GetImagePaths(path);
+            }
+            HashLayer.Sub1();
+        }
+        
+        public static void GetNumberOfInputPaths()
+        {
+            Console.WriteLine("how many paths do you want to scan?");
+            string? NumPathsInput = Console.ReadLine();
+
+            //check if the user inpout is empty or have white space
+            if (string.IsNullOrWhiteSpace(NumPathsInput))
+            {
+                Console.WriteLine("Input cannot be empty. Exiting.");
+                return;
+            }
+
+            // Convert the user input to an integer
+            if (!int.TryParse(NumPathsInput, out int numPaths) || numPaths < 1)
+            {
+                Console.WriteLine("Invalid number of paths. Exiting.");
+                return;
+            }
+
+            for (int i = 0; i < numPaths; i++)
+            {
+                Console.WriteLine("Enter target directory or file path:");
+                string? input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Input cannot be empty. Please enter a valid path.");
+                    i--;
+                    continue;
+                }
+                InputPath.Add(input);
+            }
+        }
+
+
+        public static void GetImagePaths(string rootPath)
+        {
+            //for single file
+            if (File.Exists(rootPath))
+            {
+                ProcessFile(rootPath);
+                return;
+            }
+
+            //for invalid path
+            if (!Directory.Exists(rootPath))
+            {
+                Console.WriteLine($"Invalid path: {rootPath}");
+                return;
+            }
+
+            //for directory
+            Stack<string> dirs = new Stack<string>();
+            dirs.Push(rootPath);
+
+            while (dirs.Count > 0)
+            {
+                string currentDir = dirs.Pop();
+
+                try
+                {
+                    foreach (var file in Directory.EnumerateFiles(currentDir))
+                        ProcessFile(file);
+
+                    foreach (var dir in Directory.EnumerateDirectories(currentDir))
+                        dirs.Push(dir);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error processing directory {currentDir}: {ex.Message}");
+                }
+            }
+        }
+
+
+
+
+
+        // Insert logic for processing found files here.
+        public static void ProcessFile(string path)
+        {
+            // This is where we will check if the file is an image file, if it is we will add it to the list of image paths
+            if (IsImageFile(path))
+            {
+                ImagePath?.Add(path);
+            }
+
+        }
+        public static void printImagePaths()
+        {
+            //this is for tesing purposes
+            for (int i = 0; i < ImagePath.Count; i++)
+            {
+                Console.WriteLine(i + " " + ImagePath[i]);
+            }
+        }
+        public static bool IsImageFile(string path)
+        {
+            //it is the main condition checker to determine i.f the file type is what is authorized or not
+            string extension = Path.GetExtension(path).ToLower();
+            return extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".bmp";
+            //.jpg .jpeg .png .bmp
+
+
+        }
+        public static IReadOnlyList<string> GetData()
+        {
+            return ImagePath.AsReadOnly();
+        }
+    }
+}
